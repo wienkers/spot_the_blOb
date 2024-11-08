e7483a2cd39cd2d3b53f24f35fabdb990102a4ea
Aaron Wienkers <aaron.wienkers@gmail.com>
2024-11-08 16:25:30 +0100

    renaming

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 
 setup(
-    name='pyMHW_tracker',
+    name='spot_the_blOb',
     use_scm_version={
-        "write_to": "ocetrac_dask/_version.py",
+        "write_to": "spot_the_blOb/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     }
